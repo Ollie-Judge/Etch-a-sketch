72df7d5aa2e72de42c8974a92f51bdf57820352d
Ollie-Judge <ollie.judge@hotmail.com>
2023-11-17 16:13:53 +0000

    made clickable divs and made them into grid

---
+++ script.js
@@ -0,0 +1,29 @@
+let drawingBoxesContainer = document.getElementById("drawingBoxesContainer");
+
+let userDivChoice = 16; //document.getElementById('userDivChoice')
+let userColourChoice = "red";
+
+const generateDivs = (userDivChoice) => {
+  for (let i = 0; i < userDivChoice; i++) {
+    let drawingContainer = document.createElement("div");
+
+    drawingContainer.appendChild(document.createTextNode(i));
+    drawingContainer.classList = `drawingContainer drawingContainer${i}`;
+
+    drawingBoxesContainer.appendChild(drawingContainer);
+
+    drawingContainer.addEventListener("click", function () {
+      drawingContainer.style.backgroundColor = userColourChoice;
+    });
+
+    //div.classname = "block";
+  }
+};
+
+generateDivs(userDivChoice);
+
+// once divs are created, just add a click event to all of the divs so that when each one is clicked, you then colour the div the colour you selected
+
+// for (let i = 0; i < 16; i++) {
+//   console.log(i);
+// }
